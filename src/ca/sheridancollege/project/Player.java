06cd96928fa48351969d062ea25c28aef58ad7b1
Henry Tran <tranhenr@sheridancollege.ca>
2023-04-10 16:16:54 -0400

    Final draft except testing (War Game). V1

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,49 +1,40 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
 package ca.sheridancollege.project;
 
 /**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
+ * @authors zohair, henry, manh, smith
  */
-public abstract class Player {
 
-    private String name; //the unique name for this player
+/**
+ * This class represents the player 
+ */
+public class Player {
 
-    /**
-     * A constructor that allows you to set the player's unique ID
-     *
-     * @param name the unique ID to assign to this player.
-     */
+    private String name;
+    private Hand hand;
+
+    // Constructor to initialize a new player with a given name
     public Player(String name) {
         this.name = name;
     }
-
-    /**
-     * @return the player name
-     */
+    
+    // Getter method to return the name of the player
     public String getName() {
-        return name;
+        return this.name;
     }
-
-    /**
-     * Ensure that the playerID is unique
-     *
-     * @param name the player name to set
-     */
+    
+    // Setter method to set the name of the player
     public void setName(String name) {
         this.name = name;
     }
-
-    /**
-     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
-     * with logic to play your game.
-     */
-    public abstract void play();
-
+    
+    // Getter method to return the hand of the player
+    public Hand getHand() {
+        return this.hand;
+    }
+    
+    // Setter method to set the hand of the player
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
 }
+
