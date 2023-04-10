00a1d46032d0e7de7ff695fc184d44d61459c049
mdzohairansari <ansarizo@sheridancollege.ca>
2023-04-10 14:54:42 -0400

    almost final draft of the war game1

---
+++ src/ca/sheridancollege/project/PlayGame.java
@@ -0,0 +1,9 @@
+
+import ca.sheridancollege.project.Game;
+
+public class PlayGame {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.startGame();
+    }
+}
